16297cd9393834d49ffc5a1b7a6af3a1f52bb65a
Rahul Mahajan <rahulmahajan2810@gmail.com>
2020-07-01 03:56:32 +0530

    Images added

--- psutil_client.py
+++ psutil_client.py
@@ -81,15 +81,17 @@ my_msg = tkinter.StringVar()  # For the messages to be sent.
 my_msg.set("Type your name here.")
 scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
 # Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+msg_list = tkinter.Listbox(messages_frame, height=15, width=250, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack() 
  
 entry_field = tkinter.Entry(top, textvariable=my_msg)
-entry_field.bind("<Return>", send) entry_field.pack() s
+entry_field.bind("<Return>", send)
+entry_field.pack() 
 end_button = tkinter.Button(top, text="Send", command=send)
+send_button = tkinter.Button(top, text="Send", command=send)
 send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
